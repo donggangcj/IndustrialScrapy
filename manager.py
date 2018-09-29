9ca1b1533ef1bd950d683c944fc8aaa20e1ed2f5
Dong Gang <dong.gang@daocloud.io>
2018-09-29 13:38:03 +0800

    add the run file

--- manager.py
+++ manager.py
@@ -6,3 +6,18 @@
 @Date  : 2018/9/29
 @Desc  : 
 '''
+
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import  get_project_settings
+
+"""Use the scrapy.crawler.CrawlerProcess to run the Spiders in a process simultaneously"""
+
+def run_crawer_single_prcess():
+    process = CrawlerProcess(get_project_settings())
+    process.crawl('aii_alliance')
+    process.crawl('beijing')
+    process.start()
+
+
+
