9a4da5dd3655b9b1f4d83e2b071a7d9f095fc708
Dong Gang <dong.gang@daocloud.io>
2018-10-30 17:42:06 +0800

    Add the database and init the user document

---
+++ IndustrialScrapy/restservice/model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File  : model.py
+@Author: donggangcj
+@Date  : 2018/10/30
+@Desc  : 
+'''
+import mongoengine
+from mongoengine import StringField, DateTimeField, EmbeddedDocumentField, EmailField, Fi
+from werkzeug.security import generate_password_hash, check_password_hash
+
+mongoengine.connect('mongoengine_test', host='localhost', port=27017)
+
+
+class Contact(mongoengine.EmbeddedDocument):
+    email = StringField()
+    iphone = EmailField()
+
+
+class User(mongoengine.Document):
+    username = StringField(required=True)
+    age = DateTimeField(required=True)
+    password_hash = StringField(required=True)
+    contact = EmbeddedDocumentField(Contact)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File  : config.py
+@Author: donggangcj
+@Date  : 2018/10/30
+@Desc  : 
+'''
+
+
+class Config:
+    pass
